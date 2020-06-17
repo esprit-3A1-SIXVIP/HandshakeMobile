4ebebd346bf8f3768589b73b0eed29caba7b5f3f
Soreilla <soreilla.djocgouetchaleu@esprit.tn>
2020-06-17 08:20:02 +0100

    Premier Commit Soreilla

--- src/com/mycompany/myapp/utils/Statics.java
+++ src/com/mycompany/myapp/utils/Statics.java
@@ -7,8 +7,14 @@ package com.mycompany.myapp.utils;
 
 /**
  *
- * @author bhk
+ * @author Soreilla
  */
 public class Statics {
-    public static final String BASE_URL="http://localhost/HandshakeWebSym-Samyra/web/app_dev.php";    
+        public static final String BASE_URL="http://127.0.0.1:8000/task";
+         public static final String UPDATE_URL="http://127.0.0.1:8000/taskUpdate";
+        public static final String SHOW_URL="http://127.0.0.1:8000/taskShow";
+       
+        public static final String DELETE_URL="http://127.0.0.1:8000/delete/";
+
+    
 }
