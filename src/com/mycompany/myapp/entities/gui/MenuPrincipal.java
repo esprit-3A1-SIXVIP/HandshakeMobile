a1f16e70c1112a8b803220d52ecbcde19c8a598c
AlphaMalek <malek.taktak@esprit.tn>
2020-06-18 10:06:06 +0100

    Cryptage de password

--- src/com/mycompany/myapp/entities/gui/MenuPrincipal.java
+++ src/com/mycompany/myapp/entities/gui/MenuPrincipal.java
@@ -12,11 +12,14 @@ import com.mycompany.myapp.entities.gui.Aide.MenuAide;
 import com.mycompany.myapp.entities.gui.Beneficiaire.MenuBen;
 import com.codename1.ui.Button;
 import com.codename1.ui.Dialog;
+import static com.codename1.ui.Dialog.TYPE_INFO;
+import com.codename1.ui.EncodedImage;
 import com.codename1.ui.FontImage;
 import com.codename1.ui.Form;
 import com.codename1.ui.Image;
 import com.codename1.ui.Label;
 import com.codename1.ui.TextField;
+import com.codename1.ui.URLImage;
 import com.codename1.ui.events.ActionEvent;
 import com.codename1.ui.events.ActionListener;
 import com.codename1.ui.layouts.BoxLayout;
@@ -32,6 +35,7 @@ import java.io.IOException;
 public class MenuPrincipal extends Form {
 
     Form current;
+    private EncodedImage ec = null;
     private ImageViewer iv = null;
     String cnx = "http://localhost/mobileshakehub/cnx.php";
 
@@ -61,7 +65,7 @@ public class MenuPrincipal extends Form {
                 requete1.setUrl(cnx);
                 requete1.setPost(false);
                 requete1.addArgument("email", login.getText());
-                requete1.addArgument("password", password.getText());
+                requete1.addArgument("pass", password.getText());
                 NetworkManager.getInstance()
                         .addToQueue(requete1);
                 requete1.addResponseListener(new ActionListener() {
@@ -71,10 +75,30 @@ public class MenuPrincipal extends Form {
                         if (res1.equals("error")) {
                             Dialog.show("Error", "Bad credentials", "Ok", null);
                         } else {
+                            
+
                             Form S = new Form("HandShake", BoxLayout.y());
                             User U = new User();
-
+                            
                             U.parse(res1);
+                            try {
+                Label imageDialogue;
+                try {
+                                    ec = EncodedImage.create("/load.jpg");
+                                } catch (IOException ex) {
+                                    System.out.println(ex.getMessage());
+                                }
+                if (!(U.getProfil().equals(""))) {
+                    imageDialogue = new Label(URLImage.createToStorage(ec, "http://localhost/mobileshakehub/"+U.getProfil(), "http://localhost/mobileshakehub/"+U.getProfil(), URLImage.RESIZE_SCALE));
+                } else {
+                    imageDialogue = new Label(Image.createImage("/usericon.png").scaled(300, 300));
+                }
+                imageDialogue.setText(("Logged in successfully as " + U.getPrenomUser() + " " + U.getNomUser()) + " !");
+                imageDialogue.setTextPosition(Label.BOTTOM);
+                Dialog.show("Welcome to the ShakeHub", imageDialogue, null, TYPE_INFO, null, 2000);
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
                             //BUTTONS
                             add(new Label("Choisissez une option"));
                             Button btnAide = new Button("Gestions des Aides");
